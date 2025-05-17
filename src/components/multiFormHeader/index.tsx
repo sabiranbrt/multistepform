a96fdaf147546d73fc98ebb8ffff791a2f7d4601
sabiranshah <sabiran.brt@gmail.com>
2025-05-17 18:38:21 +0545

    echo password issue soled , taiwind issue need to be fixed

--- src/components/multiFormHeader/index.tsx
+++ src/components/multiFormHeader/index.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { JsonFormat } from "../../utils/usejsonStructure";
+import { TiTick } from "react-icons/ti";
 
 interface IProp {
   currentIndex: number;
@@ -8,29 +9,44 @@ interface IProp {
 const MultiFormHeader = ({ currentIndex }: IProp) => {
   return (
     <div className="  my-12 relative ">
-      <div className="flex flex-col gap-20 items-start justify-between w-full !py-10">
+      <div className="flex flex-col gap-20 items-end justify-between w-full !py-10">
         {JsonFormat.map((item, index) => {
           const activeIndex = index === currentIndex;
+          const isComplete = index < currentIndex;
           return (
-            <div className="inline-flex gap-2.5 items-center">
-              <div
+            <div key={index} className="inline-flex gap-5 items-center">
+              <p
                 className={clsx(
-                  " w-10 h-10 shadow-md shadow-gray-300 rounded-md flex items-center justify-center",
-                  activeIndex ? "bg-[#0D77FC]" : "bg-[#FFF2F2]"
+                  "font-semibold text-[18px] ",
+                  isComplete ? "text-[#5081B9]" : "text-gray-500"
                 )}
               >
-                <p className={clsx(activeIndex ? "text-white" : "text-black")}>
-                  {index + 1}
-                </p>
-              </div>
-              <p className=" font-semibold text-[20px] text-gray-700">
                 {item.status}
               </p>
+              <div
+                className={clsx(
+                  " w-8 h-8 shadow-md shadow-gray-300 rounded-full flex items-center justify-center",
+                  activeIndex || isComplete ? "bg-[#5081B9]" : "bg-[#FFF2F2]"
+                )}
+              >
+                {isComplete ? (
+                  <TiTick color="white" />
+                ) : (
+                  <p
+                    className={clsx(
+                      "font-bold text-[15px]",
+                      activeIndex ? "text-white" : "text-black"
+                    )}
+                  >
+                    {index + 1}
+                  </p>
+                )}
+              </div>
             </div>
           );
         })}
       </div>
-      <div className=" absolute top-0 right-[9px] w-[0.5px] h-full bg-gray-300"></div>
+      {/* <div className=" absolute top-0 right-0 w-[0.5px] h-full bg-gray-300"></div> */}
     </div>
   );
 };
